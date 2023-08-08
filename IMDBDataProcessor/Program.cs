4549e6a84fcd7807e605739762e3b62d175d7a1f
Kevin Morris <kmorris511@gmail.com>
2023-08-07 18:16:21 -0600

    setting up command line menus

--- IMDBDataProcessor/Program.cs
+++ IMDBDataProcessor/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IMDBDataProcessor;
+
+var input = -1;
+
+while (input != 0)
+{
+    Console.WriteLine("Main Menu");
+    Console.WriteLine("----------------------");
+    Console.WriteLine("1. Import Movies");
+    Console.WriteLine("2. Import Crew");
+    Console.WriteLine("3. Export Data to Flat Files");
+    Console.WriteLine("0. Exit");
+    Console.Write("> ");
+    input = int.Parse(Console.ReadLine());
+    switch (input)
+    {
+        case 1:
+            Console.WriteLine("Importing Movies...");
+            MovieImporter.Import();
+            break;
+        case 2:
+            Console.WriteLine("Importing Crew...");
+            CrewImporter.Import();
+            break;
+        case 3:
+            Console.WriteLine("Exporting Data to Flat Files...");
+            FlatFileExporter.Export();
+            break;
+        case 0:
+            Console.WriteLine("Exiting...");
+            break;
+        default:
+            Console.WriteLine("Invalid Selection");
+            break;
+    }
+}
